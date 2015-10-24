62076ae1be17d8b674d590e9f33bc380dee3102e
Yury Korchemkin <yuri.korchyomkin@gmail.com>
2015-10-23 17:58:49 -0700

    change to avoid 'duplicate extensions' for some filetypes (doesn't work for .jpg though)

--- server/default.js
+++ server/default.js
@@ -6,16 +6,20 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/..'));
 app.use('/uploaded-files', express.static(__dirname + '/uploaded-files'));
-server.listen(process.env.PORT || 3000);
+var listener = server.listen(process.env.PORT || 3000);
 
 app.post('/upload', $multipart(), function(request, response, next) {
-    console.log(request.files);
+    //console.log(request.files);
     var file = request.files.file;
         console.log(file);
         var pathParts = file.path.split(/[\/\\]/);
-        var extension = file.type.split(/\//)[1];
-        var targetPath = "/uploaded-files/"+pathParts[pathParts.length - 1]+"."+extension;
-        console.log(targetPath);
+        var extension = "."+file.type.split(/\//)[1];
+        var filename = pathParts[pathParts.length - 1];
+        var targetPath = "/uploaded-files/"+filename;
+        if (!filename.match(extension+"$")){
+            targetPath = targetPath+extension;
+        }
         fs.copySync(file.path, __dirname+targetPath);
+        console.log("http://localhost:" + listener.address().port+targetPath);
     response.send({ path: targetPath, success: true });
 });
