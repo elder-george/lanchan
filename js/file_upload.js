b1d0aa045d3e627cb6ec7eda1d2759805d7c071f
Yury Korchemkin <yury.korchemkin@gmail.com>
2025-11-01 16:08:35 -0700

    unify path handling

--- js/file_upload.js
+++ js/file_upload.js
@@ -1,114 +1,114 @@
-(function($){
+(function ($) {
 
-function uploadFile(results){
-  var fd = new FormData();
-  fd.append("file", results.file);
-  $.ajax({
-        type: "POST",
-        url:"/upload",
-        data: fd,
-        processData: false,
-        contentType:false,
-        success: function(e){ 
-            var permalink = $("#permalink");
-            var url = window.location.protocol + "//" + window.location.host + e.path;
-            permalink.attr('href', url);
-            permalink.text(url);
-            if (window.localStorage){
-                window.localStorage.setItem(new Date(), JSON.stringify({ url:url}));
-                populateHistory($history, window.localStorage);
-            }
-        }
-  });
-}
-
-function deleteFile(url, onSuccess){
-    $.ajax({
-        type: "DELETE",
-        url: "/upload/"+url,
-        success: onSuccess
-    });
-}
-
-function populateHistory(history, items){
-    history.empty();
-    for(var key in items){
-        var data = JSON.parse(items[key]);
-        if (! data) continue;
-        var fileName = data.url.split('/').pop();
-        var $historyItem = $('<li/>')
-            .append([
-                $('<a/>').text(moment(key).format("MM DD, hh:mm:ss")).attr('href', data.url),
-                $('<a/>').attr('class', 'del').attr('url', fileName).attr('title', 'delete this file?').text('[X]')
-            ]);
-        $historyItem.prependTo($history);
-
-        $historyItem.find('a.del').on('click', function(e){
-            var $this = $(this);
-            var url = $this.attr('url');
-            deleteFile(url, function(){
-                $this.closest('li').remove();
-                if (window.localStorage){
-                    window.localStorage.removeItem(key);
+    function uploadFile(results) {
+        var fd = new FormData();
+        fd.append("file", results.file);
+        $.ajax({
+            type: "POST",
+            url: "/upload",
+            data: fd,
+            processData: false,
+            contentType: false,
+            success: function (e) {
+                var permalink = $("#permalink");
+                var url = window.location.protocol + "//" + window.location.host + '/' + e.path;
+                permalink.attr('href', url);
+                permalink.text(url);
+                if (window.localStorage) {
+                    window.localStorage.setItem(new Date(), JSON.stringify({ url: url }));
+                    populateHistory($history, window.localStorage);
                 }
-            })
+            }
         });
     }
-}
+
+    function deleteFile(filename, onSuccess) {
+        $.ajax({
+            type: "DELETE",
+            url: "/upload/" + filename,
+            success: onSuccess
+        });
+    }
+
+    function populateHistory(history, items) {
+        history.empty();
+        for (var key in items) {
+            var data = JSON.parse(items[key]);
+            if (!data) continue;
+            var fileName = data.url.split('/').pop();
+            var $historyItem = $('<li/>')
+                .append([
+                    $('<a/>').text(moment(key).format("MM DD, hh:mm:ss")).attr('href', data.url),
+                    $('<a/>').attr('class', 'del').attr('url', fileName).attr('title', 'delete this file?').text('[X]')
+                ]);
+            $historyItem.prependTo($history);
+
+            $historyItem.find('a.del').on('click', function (e) {
+                var $this = $(this);
+                var url = $this.attr('url');
+                deleteFile(url, function () {
+                    $this.closest('li').remove();
+                    if (window.localStorage) {
+                        window.localStorage.removeItem(key);
+                    }
+                })
+            });
+        }
+    }
 
 
 
-$("html").pasteImageReader(function(results) {
-  var dataURL, filename;
-  filename = results.filename, dataURL = results.dataURL;
-  $data.text(dataURL);
-  $size.val(results.file.size);
-  $type.val(results.file.type);
-  $test.attr('href', dataURL);
-  var img = document.createElement('img');
-  img.src= dataURL;
-  var w = img.width;
-  var h = img.height;
-  $width.val(w)
-  $height.val(h);
-  uploadFile(results);
-  return $(".active").css({
-    backgroundImage: "url(" + dataURL + ")"
-  }).data({'width':w, 'height':h});
-});
+    $("html").pasteImageReader(function (results) {
+        var dataURL, filename;
+        filename = results.filename, dataURL = results.dataURL;
+        $data.text(dataURL);
+        $size.val(results.file.size);
+        $type.val(results.file.type);
+        $test.attr('href', dataURL);
+        var img = document.createElement('img');
+        img.src = dataURL;
+        var w = img.width;
+        var h = img.height;
+        $width.val(w)
+        $height.val(h);
+        uploadFile(results);
+        return $(".active").css({
+            backgroundImage: "url(" + dataURL + ")"
+        }).data({ 'width': w, 'height': h });
+    });
 
-$('html').fileDrop({
-    dragOver: function(e){
-        debugger;
-    },
-    onFileRead: function(e){
-        results = e[0];
-       uploadFile(results);
-      var img = document.createElement('img');
-      img.src= results.dataURL;
-      var w = img.width;
-      var h = img.height;
-       return $(".target").css({
-                    backgroundImage: "url(" + results.dataURL + ")",
-                    width:w, height:h,
-                  }).data({'width':w, 'height':h});
-    },
+    $('html').fileDrop({
+        dragOver: function (e) {
+            debugger;
+        },
+        onFileRead: function (e) {
+            results = e[0];
+            uploadFile(results);
+            var img = document.createElement('img');
+            img.src = results.dataURL;
+            var w = img.width;
+            var h = img.height;
+            return $(".target").css({
+                backgroundImage: "url(" + results.dataURL + ")",
+                width: w, height: h,
+            }).data({ 'width': w, 'height': h });
+        },
 
-    removeDataUriScheme: true,
-    decodeBase64: true
-});
+        removeDataUriScheme: true,
+        decodeBase64: true
+    });
 
 
-var $data, $size, $type, $test, $width, $height, $history;
-$(function() {
-  $data = $('.data');
-  $size = $('.size');
-  $type = $('.type');
-  $test = $('#test');
-  $width = $('#width');
-  $height = $('#height');
-  $history = $("#history");
-  if (window.localStorage)
-    populateHistory($history, window.localStorage)
-})
+    var $data, $size, $type, $test, $width, $height, $history;
+    $(function () {
+        $data = $('.data');
+        $size = $('.size');
+        $type = $('.type');
+        $test = $('#test');
+        $width = $('#width');
+        $height = $('#height');
+        $history = $("#history");
+        if (window.localStorage)
+            populateHistory($history, window.localStorage)
+    })
 })($);
--- server/default.js
+++ server/default.js
@@ -1,40 +1,42 @@
 import express from 'express';
 import path from 'node:path';
+import pathPosix from 'node:path/posix';
 import fs from 'node:fs';
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, } from 'node:url';
 import http from 'http';
 import multer from 'multer';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const server = http.createServer(app);
-const targetDir = dirname + '/uploaded-files/';
+const targetDir = 'uploaded-files/';
 
 const listener = server.listen(process.env.PORT || 3000);
 app.use(express.static(dirname + '/..'))
     .use('/uploaded-files', express.static(targetDir))
-    .post('/upload', multer({ dest: targetDir }).single('file'), function (request, response) {
+    .post('/upload', multer({ dest: targetDir }).single('file'), function (request, response, next) {
         const file = request.file;
         console.log(file);
-        const extension = "." + file.mimetype.split(/\//)[1];
+        const extension = path.extname(file.originalname);
         const filename = file.filename;
-        const targetPath = "/uploaded-files/" + filename;
-        if (!(filename.match(extension + "$") || filename.match(".jpg$"))) {
-            targetPath = targetPath + extension;
-        }
-        fs.copyFileSync(file.path, dirname + targetPath);
-        console.log("http://localhost:" + listener.address().port + targetPath);
+        const targetFileName = [extension, 'jpg'].includes(path.extname(filename))
+            ? filename
+            : filename + extension;
+        const targetPath = pathPosix.join(targetDir, targetFileName);
+        fs.copyFileSync(file.path, targetPath);
+        console.log("http://localhost:" + listener.address().port + '/' + targetPath);
         response.send({ path: targetPath, success: true });
-    }).delete('/upload/:url', function (request, response) {
-        console.log('deleting ' + request.url);
+        next();
+    }).delete('/upload/:filename', function (request, response, next) {
+        console.log('deleting ' + request.path);
         try {
-            let path = targetDir + request.url.split('/').pop();
-            path = path.replace(/\*\?\+/, '');
-            fs.unlinkSync(path);
+            const filePath = pathPosix.join(targetDir, request.params.filename);
+            fs.unlinkSync(filePath);
             console.log('deleted!');
             response.send({ success: true });
         } catch (e) {
             console.log("couldn't delete: " + e);
             response.send({ success: false });
         }
+        next();
     });
