c3a04713341c78fb0f4aed470e8ca0eab733ad74
Yury Korchemkin <yury.korchemkin@gmail.com>
2025-11-01 15:09:43 -0700

    make file a JS module

--- server/default.js
+++ server/default.js
@@ -1,39 +1,40 @@
-var express = require('express'),
-    app = express(),
-    fs = require('fs-extra'),
-    server = require('http').createServer(app),
-    $multipart = require('connect-multiparty');
+import express from 'express';
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import http from 'http';
+import multer from 'multer';
 
-app.use(express.static(__dirname + '/..'));
-app.use('/uploaded-files', express.static(__dirname + '/uploaded-files'));
-var listener = server.listen(process.env.PORT || 3000);
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const server = http.createServer(app);
+const targetDir = dirname + '/uploaded-files/';
 
-app.post('/upload', $multipart(), function (request, response, next) {
-    //console.log(request.files);
-    var file = request.files.file;
-    console.log(file);
-    var pathParts = file.path.split(/[\/\\]/);
-    var extension = "." + file.type.split(/\//)[1];
-    var filename = pathParts[pathParts.length - 1];
-    var targetPath = "/uploaded-files/" + filename;
-    if (!(filename.match(extension + "$") || filename.match(".jpg$"))) {
-        targetPath = targetPath + extension;
-    }
-    fs.copySync(file.path, __dirname + targetPath);
-    console.log("http://localhost:" + listener.address().port + targetPath);
-    response.send({ path: targetPath, success: true });
-});
-
-app.delete('/upload/:url', function (request, response) {
-    console.log('deleting ' + request.url);
-    try {
-        var path = __dirname + "/uploaded-files/" + request.url.split('/').pop();
-        path = path.replace(/\*\?\+/, '');
-        fs.unlinkSync(path);
-        console.log('deleted!');
-        response.send({ success: true });
-    } catch (e) {
-        console.log("couldn't delete: " + e);
-        response.send({ success: false });
-    }
-});
+const listener = server.listen(process.env.PORT || 3000);
+app.use(express.static(dirname + '/..'))
+    .use('/uploaded-files', express.static(targetDir))
+    .post('/upload', multer({ dest: targetDir }).single('file'), function (request, response) {
+        const file = request.file;
+        console.log(file);
+        const extension = "." + file.mimetype.split(/\//)[1];
+        const filename = file.filename;
+        const targetPath = "/uploaded-files/" + filename;
+        if (!(filename.match(extension + "$") || filename.match(".jpg$"))) {
+            targetPath = targetPath + extension;
+        }
+        fs.copyFileSync(file.path, dirname + targetPath);
+        console.log("http://localhost:" + listener.address().port + targetPath);
+        response.send({ path: targetPath, success: true });
+    }).delete('/upload/:url', function (request, response) {
+        console.log('deleting ' + request.url);
+        try {
+            let path = targetDir + request.url.split('/').pop();
+            path = path.replace(/\*\?\+/, '');
+            fs.unlinkSync(path);
+            console.log('deleted!');
+            response.send({ success: true });
+        } catch (e) {
+            console.log("couldn't delete: " + e);
+            response.send({ success: false });
+        }
+    });
